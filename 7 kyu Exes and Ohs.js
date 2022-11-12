ed168acbd45293e1a95d0386b415d82fdff1612a
Andres Gonzalez <106206361+andresgonzalezarbildi@users.noreply.github.com>
2022-11-11 22:40:44 -0300

    Create 7 kyu Exes and Ohs.js

---
+++ 7 kyu Exes and Ohs.js
@@ -0,0 +1,25 @@
+/*
+
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+
+*/
+
+function XO(str) {
+    let res = 0
+    for (let letter of str) {
+      if (letter == 'x' || letter == 'X') {
+        res++
+      } else if(letter == 'o' || letter == 'O') {
+        res--
+      }
+    }
+  return res == 0
+}
