32018e8a140b7f8b6b5d37afd49411b46f7bbaeb
Andres Gonzalez <106206361+andresgonzalezarbildi@users.noreply.github.com>
2022-12-24 11:29:03 -0300

    Create 8 kyu Exclamation marks series #1: Remove an exclamation mark from the end of string.js

---
+++ 8 kyu Exclamation marks series #1: Remove an exclamation mark from the end of string.js
@@ -0,0 +1,22 @@
+/*
+
+Description:
+
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+Examples
+
+remove("Hi!") == "Hi"
+remove("Hi!!!") == "Hi!!"
+remove("!Hi") == "!Hi"
+remove("!Hi!") == "!Hi"
+remove("Hi! Hi!") == "Hi! Hi"
+remove("Hi") == "Hi"
+
+*/
+
+function remove (string) {
+  if(string[string.length -1]==="!"){
+    return string.slice(0, string.length -1)
+  }
+  return string
+}
