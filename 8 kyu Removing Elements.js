c044f7062d6673fa9f7b1138058ddd6550129222
Andres Gonzalez <106206361+andresgonzalezarbildi@users.noreply.github.com>
2023-01-08 12:32:00 -0300

    Create 8 kyu Removing Elements.js

---
+++ 8 kyu Removing Elements.js
@@ -0,0 +1,16 @@
+/*
+
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+Example:
+
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+
+*/
+
+function removeEveryOther(arr){
+  return arr.filter((x,i) => {
+    if(i%2 == 0) return x;
+  })
+}
