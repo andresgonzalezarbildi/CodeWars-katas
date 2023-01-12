75a91cd5c800bcbfe5bd4379de2e2d29326f583c
Andres Gonzalez <106206361+andresgonzalezarbildi@users.noreply.github.com>
2023-01-12 11:29:29 -0300

    Create 7 kyu Small enough? - Beginner.js

---
+++ 7 kyu Small enough? - Beginner.js
@@ -0,0 +1,14 @@
+/*
+
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+
+*/
+
+function smallEnough(a, limit){
+  for(let num of a){
+    if(num > limit) return false;
+  }
+  return true;
+}
