3b8ebc80423e76c921086f156b604c35ae15623a
Andres Gonzalez <106206361+andresgonzalezarbildi@users.noreply.github.com>
2023-02-08 08:10:40 -0300

    Create 7 kyu Find the capitals.js

---
+++ 7 kyu Find the capitals.js/7 kyu Find the capitals.js
@@ -0,0 +1,20 @@
+/*
+
+Instructions
+
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+Example
+
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+*/
+
+var capitals = function (word) {
+  let res = []
+	for (let letters of word) {
+    if (letters === letters.toUpperCase()){
+      res.push(word.indexOf(letters))
+    }
+  }
+  return res
+};
