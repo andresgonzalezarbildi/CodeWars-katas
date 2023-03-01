56a666a13629251a316751c393336a7de0aa91ef
Andres Gonzalez <106206361+andresgonzalezarbildi@users.noreply.github.com>
2023-03-01 08:12:50 -0300

    Create 8 kyu Alan Partridge II - Apple Turnover.js

---
+++ 8 kyu Alan Partridge II - Apple Turnover.js
@@ -0,0 +1,18 @@
+/*
+
+Alan is known for referring to the temperature of the apple turnover as Hotter than the sun!. According to space.com the temperature of the sun's corona is 2,000,000 degrees Celsius, but we will ignore the science for now.
+Task
+
+Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+Note: Input will either be a positive integer (or a string for untyped languages).
+Other katas in this series:
+
+Alan Partridge I - Partridge Watch
+Alan Partridge III - London
+
+*/
+
+function apple(x){
+return x**2 > 1000? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+}
