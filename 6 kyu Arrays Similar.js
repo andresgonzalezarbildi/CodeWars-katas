7d199295185d91d14da6209863b0df9b4ee77399
Andres Gonzalez <106206361+andresgonzalezarbildi@users.noreply.github.com>
2023-03-16 13:31:34 -0300

    Create 6 kyu Arrays Similar.js

---
+++ 6 kyu Arrays Similar.js
@@ -0,0 +1,18 @@
+/*
+
+Write a function that determines whether the passed in sequences are similar. Similar means they contain the same elements, and the same number of occurrences of elements.
+
+var arr1 = [1, 2, 2, 3, 4],
+    arr2 = [2, 1, 2, 4, 3],
+    arr3 = [1, 2, 3, 4],
+    arr4 = [1, 2, 3, "4"]
+
+arraysSimilar(arr1, arr2); // Should equal true
+arraysSimilar(arr2, arr3); // Should equal false
+arraysSimilar(arr3, arr4); // Should equal false
+
+*/
+
+function arraysSimilar(arr1, arr2) {
+  return ((arr1.length === arr2.length) && arr1.sort().every((val, index) => val === arr2.sort()[index]))
+}
