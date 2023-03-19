50caca9afb7d0ea9e06405d27106ac1387cb15cb
Andres Gonzalez <106206361+andresgonzalezarbildi@users.noreply.github.com>
2023-03-19 12:22:19 -0300

    Create 6 kyu Array Deep Count.js

---
+++ 6 kyu Array Deep Count.js
@@ -0,0 +1,22 @@
+/*
+
+You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+Examples
+
+[]                   -->  0
+[1, 2, 3]            -->  3
+["x", "y", ["z"]]    -->  4
+[1, 2, [3, 4, [5]]]  -->  7
+
+The input will always be an array.
+
+*/
+
+function deepCount(a){
+  let result = 0;
+  for ( let i = 0; i<a.length; i++) {
+    if (Array.isArray(a[i])) result += deepCount(a[i]);
+    result++;
+  }
+  return result;
+}
