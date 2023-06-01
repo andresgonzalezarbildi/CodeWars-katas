2105bdf1cd363b3315958721bac9c52fdbe835ea
Andres Gonzalez <106206361+andresgonzalezarbildi@users.noreply.github.com>
2023-06-01 16:06:27 -0300

    Create 8 kyu Are arrow functions odd?.js

---
+++ 8 kyu Are arrow functions odd?.js
@@ -0,0 +1,12 @@
+/*
+
+Time to test your basic knowledge in functions! Return the odds from a list:
+
+[1, 2, 3, 4, 5]  -->  [1, 3, 5]
+[2, 4, 6]        -->  []
+
+*/
+
+function odds(values){
+  return values.filter( x => x % 2 !== 0 );
+}
